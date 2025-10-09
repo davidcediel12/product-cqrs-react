105b67b51d8316c7f5ca9a6f31313b893d9abbe4
David Cediel <esdago18@gmail.com>
2025-10-08 22:09:47 -0500

    Improve UI for the main menu

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,13 @@ import './App.css'
 function App() {
   return (
 
-    <div className='flex items-center justify-center w-full h-screen'>
+    <div className='flex items-center justify-center w-full h-screen bg-gradient-to-br from-blue-50 to-indigo-100'>
 
-      <div className="flex flex-col w-1/4 h-1/2 bg-gray-200/50 border-gray-700 items-center justify-between rounded-lg">
-        <h2 className="mt-4 text-3xl font-bold">Options</h2>
+      <div className="flex flex-col w-1/4 h-1/2 bg-white/80 border-gray-700 items-center justify-between rounded-lg shadow-2xl">
+        <h2 className="mt-4 text-3xl font-bold text-purple-400">Options</h2>
         <div className="flex-1 flex items-center justify-center">
-          <button className="rounded bg-green-200 border-1 border-green-300 px-4 py-2 text-gray-600 font-bold hover:border-purple-400 hover:cursor-pointer">
+          <button className="rounded bg-green-200 border-1 border-green-300 px-4 py-2 text-gray-600 font-bold hover:border-purple-400 hover:cursor-pointer
+          shadow-lg hover:shadow-xl transform hover:scale-105 transition-all duration-200 border-0">
             Create Product
           </button>
         </div>
