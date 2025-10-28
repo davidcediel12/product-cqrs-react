203a974ed654ff54ae5aedbacbbf8ae73cb95652
David Cediel <esdago18@gmail.com>
2025-10-28 10:27:06 -0500

    Create custom button component in the main menu

--- src/pages/MainMenu/MainMenu.jsx
+++ src/pages/MainMenu/MainMenu.jsx
@@ -1,25 +1,31 @@
 import { Link } from "react-router-dom";
 
-
-
 export default function MainMenu() {
-    return (
+  return (
+    <div className="flex items-center justify-center w-full h-screen bg-gradient-to-br from-blue-50 to-indigo-100">
+      <div className="flex flex-col w-1/4 h-1/2 bg-white/80 border-gray-700 items-center justify-between rounded-lg shadow-2xl">
+        <h2 className="mt-4 text-3xl font-bold text-purple-400">Options</h2>
+        <div className="flex-1 flex flex-col items-center justify-center gap-3">
+          <Link to="/create-product">
+            <Button text="Create Product" />
+          </Link>
 
-        <div className='flex items-center justify-center w-full h-screen bg-gradient-to-br from-blue-50 to-indigo-100'>
-
-            <div className="flex flex-col w-1/4 h-1/2 bg-white/80 border-gray-700 items-center justify-between rounded-lg shadow-2xl">
-                <h2 className="mt-4 text-3xl font-bold text-purple-400">Options</h2>
-                <div className="flex-1 flex items-center justify-center">
-                    <Link to="/create-product">
-
-                        <button className="rounded bg-green-200 border-1 border-green-300 px-4 py-2 text-gray-600 font-bold hover:border-purple-400 hover:cursor-pointer
-              shadow-lg hover:shadow-xl transform hover:scale-105 transition-all duration-200 border-0">
-                            Create Product
-                        </button>
-                    </Link>
-                </div>
-            </div>
-
+          <Link to="/products">
+            <Button text="List products" />
+          </Link>
         </div>
-    )
+      </div>
+    </div>
+  );
+}
+
+function Button({ text }) {
+  return (
+    <button
+      className="rounded bg-green-200 border-1 border-green-300 px-4 py-2 text-gray-600 font-bold hover:border-purple-400 hover:cursor-pointer
+              shadow-lg hover:shadow-xl transform hover:scale-105 transition-all duration-200 border-0"
+    >
+      {text}
+    </button>
+  );
 }
