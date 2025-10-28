0f7052b0f00dbc467fc1b9379af2522e15914b80
David Cediel <esdago18@gmail.com>
2025-10-28 10:09:14 -0500

    Move MainMenu to it custom folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { Route, Routes } from 'react-router-dom'
 import './App.css'
-import MainMenu from './pages/MainMenu'
+import MainMenu from './pages/MainMenu/MainMenu'
 import CreateProduct from './pages/CreateProduct'
 
 function App() {
