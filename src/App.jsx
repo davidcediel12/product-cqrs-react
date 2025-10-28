e610a4c3580e647817fea87651678574d3089a0f
David Cediel <esdago18@gmail.com>
2025-10-28 10:09:27 -0500

    Create products menu component

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import MainMenu from './pages/MainMenu/MainMenu'
 import CreateProduct from './pages/CreateProduct'
+import ProductsMenu from './pages/ProductsMenu'
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<MainMenu />} />
       <Route path="/create-product" element={<CreateProduct />} />
+      <Route path="/products" element={<ProductsMenu/>} />
     </Routes>
   )
 }
